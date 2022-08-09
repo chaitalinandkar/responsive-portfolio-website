b2e9709d5026d8b6dfa3c0655ca99ff37f628117
chaitalinandkar <chaitalinandkar@gmail.com>
2022-08-09 10:40:17 -0700

    Recreated header part of home page

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Header.css"
+import ganesha from "./Images/ganesha.jpg";
 
 export default function Header() {
   return (
-    <div className="Header">
-      
-      <div className="header-details">
-        
-        <h1>Hi, I am </h1>
-        <h2>Master Aditya Varmaji</h2>
-        <p>The Best Indian Astrologer</p>
+      <div className="row">
+        <div className='col'>
+          <img src={ganesha} className='header-image img-fluid' alt='ganesha idol'></img>
+        </div>
+        <div className="col">
+          <div className="header-details">
+            <h1>Hi, I am </h1>
+            <h2>Master Aditya Varmaji</h2>
+            <p>The Best Indian Astrologer</p>
+          </div>
+          
+        </div>
       </div>
-      <div className='header-image'>
-        
-      </div>
-    </div>
   );
 }
